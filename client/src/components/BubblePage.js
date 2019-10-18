c2bc0e01d8568fe4379ca2cd3f4a409a0d135d72
Martins-O-U <martinsonyedikachi@gmail.com>
2019-10-18 09:57:54 +0100

    ColorList updated, protected routed created, data displayed on the browser

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import axiosWithAuth from "./AxiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  useEffect(() => {
+    getColors();
+  }, [])
+
+  const getColors = () => {
+    axiosWithAuth()
+    .get('/colors')
+    .then(res => {
+      setColorList(res.data)
+      console.log(res.data)})
+  }
+
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList colors={colorList} updateColors={setColorList} getColors={getColors}/>
       <Bubbles colors={colorList} />
     </>
   );
