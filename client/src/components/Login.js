2f0338899da1ae266e68f9a9d3ddf1c04402bbc2
Martins-O-U <martinsonyedikachi@gmail.com>
2019-10-18 12:00:32 +0100

    Login display edited and styled

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -22,24 +22,26 @@ const Login = (props) => {
     .then(res => {
       localStorage.setItem('token', res.data.payload);
       props.history.push('/protected');
-      console.log(res)
+      // console.log(res)
     })
     .catch(err => console.log(err.message))
   }
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
+    <div className='login'>
       <h1>Welcome to the Bubble App!</h1>
-      <h2>Please Login</h2>
-      <>
-      <form onSubmit={submitForm}>
-        <input name='username' placeholder='username' value={login.username} onChange={handleChange}/>
-        <input name='password' placeholder='password' value={login.password} onChange={handleChange}/>
+      <form onSubmit={submitForm} className='loginForm'>
+        <h3>Please Enter Your Login Details </h3>
+        <div>
+          <input name='username' placeholder='Enter username...' value={login.username} onChange={handleChange} />
+        </div>
+        <div>
+          <input name='password' placeholder='Enter Password...' value={login.password} onChange={handleChange} />
+        </div>
         <button>Login</button>
       </form>
-      </>
-    </>
+    </div>
   );
 };
 
